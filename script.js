1364edba10a76f358961c4084eefe56c2cc5ca92
Save Kim <wkdbffhl0@gmail.com>
2021-11-01 19:29:37 +0900

    update 11/01

--- script.js
+++ script.js
@@ -8,14 +8,14 @@ var image_nose_width = 27;
 var image_nose_height = 26;
 
 function noseFuncion() {
-  if (image_nose_width > 50) {
+  if (image_nose_width > 50 && sound_joygo.paused && sound_ong.paused) {
     image_nose_width = 27;
     image_nose_height = 26;
 
     sound_joygo.play();
-  } else {
-    image_nose_width += 12;
-    image_nose_height += 12;
+  } else if(sound_ong.paused) {
+    image_nose_width += 11;
+    image_nose_height += 11;
 
     sound_ong.play();
   }
