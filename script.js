87176966081a67aae9d3b3c76d344453b8b2dfbc
Save Kim <wkdbffhl0@gmail.com>
2021-11-03 01:09:33 +0900

    11/03 update

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const sound_joygo = new Audio();
-sound_joygo.src = "sound/joygo.flac";
+sound_joygo.src = "sound/sfx/gayjoygo.flac";
 const sound_ong = new Audio();
-sound_ong.src = "sound/ong.mp3";
+sound_ong.src = "sound/sfx/ong.mp3";
 
-const image_nose = document.getElementById("kane_nose_image");
+const image_nose = document.getElementById("kane_nose_image");  // got nose tag
 var image_nose_width = 27;
 var image_nose_height = 26;
+
+const roach_video = document.getElementById("meme_kane_roach_gif"); // got roach tag
 
 function noseFuncion() {
   if (image_nose_width > 50 && sound_joygo.paused && sound_ong.paused) {
@@ -22,4 +24,21 @@ function noseFuncion() {
 
   image_nose.style.width = image_nose_width + "px";
   image_nose.style.height = image_nose_height + "px";
+}
+
+///
+
+roach_video.addEventListener("mouseover", function()
+{
+  roach_video.play();
+})
+
+roach_video.addEventListener("mouseout", function()
+{
+  roach_video.pause();
+})
+
+function volume_up()
+{
+  roach_video.muted = false;
 }
