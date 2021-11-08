ccaef40aa1b3fd103d58477ca3add9f5432f4a92
Save Kim <wkdbffhl0@gmail.com>
2021-11-09 00:03:30 +0900

    11/09 update

---
+++ music-list.js
@@ -0,0 +1,14 @@
+let allMusic = [
+    {
+        name: "mungtaengi-world",
+        artist: "Yoo_Suk",
+        img: "images/music/cover/mungtaengi-world_cover.png",
+        src: "sound/bgm/Yoo_Suk-mungtaengi-world.mp3"
+    },
+    {
+        name: "gayjoygo",
+        artist: "Tajiri",
+        img: "images/music/cover/gayjoygo_cover.jpg",
+        src: "sound/bgm/Tajiri-gayjoygo.mp3"
+    }
+]
--- script.js
+++ script.js
@@ -1,44 +1,70 @@
-const sound_joygo = new Audio();
-sound_joygo.src = "sound/sfx/gayjoygo.flac";
-const sound_ong = new Audio();
-sound_ong.src = "sound/sfx/ong.mp3";
+/* 변수 선언 */
+const sfx_joygo = new Audio();
+const sfx_ong = new Audio();
+const sfx_sorry = new Audio();
+const meme_kane_bignose = document.getElementById("kane_nose_image");  // got nose tag
+const meme_roach_video = document.getElementById("meme_kane_roach_gif"); // got roach tag
+const meme_sorry_sprite = document.getElementById("meme_kane_sorry"); // got sorry tag
+var meme_kane_bignose_width = 27;
+var meme_kane_bignose_height = 26;
 
-const image_nose = document.getElementById("kane_nose_image");  // got nose tag
-var image_nose_width = 27;
-var image_nose_height = 26;
+/* 경로 선언 */
+sfx_joygo.src = "sound/sfx/gayjoygo.flac";
+sfx_ong.src = "sound/sfx/ong.mp3";
+sfx_sorry.src = "sound/sfx/sorry.mp3";
 
-const roach_video = document.getElementById("meme_kane_roach_gif"); // got roach tag
-
+// 코가 자유로운 나
 function noseFuncion() {
-  if (image_nose_width > 50 && sound_joygo.paused && sound_ong.paused) {
-    image_nose_width = 27;
-    image_nose_height = 26;
+  if (meme_kane_bignose_width > 50 && sfx_joygo.paused && sfx_ong.paused) {
+    meme_kane_bignose_width = 27;
+    meme_kane_bignose_height = 26;
 
-    sound_joygo.play();
-  } else if(sound_ong.paused) {
-    image_nose_width += 11;
-    image_nose_height += 11;
+    sfx_joygo.play();
+  } else if(sfx_ong.paused) {
+    meme_kane_bignose_width += 11;
+    meme_kane_bignose_height += 11;
 
-    sound_ong.play();
+    sfx_ong.play();
   }
 
-  image_nose.style.width = image_nose_width + "px";
-  image_nose.style.height = image_nose_height + "px";
+  meme_kane_bignose.style.width = meme_kane_bignose_width + "px";
+  meme_kane_bignose.style.height = meme_kane_bignose_height + "px";
 }
+//
 
-///
-
-roach_video.addEventListener("mouseover", function()
+// 바선생을 뵈어서 기뻐하는 나
+meme_roach_video.addEventListener("mouseover", function()
 {
-  roach_video.play();
+  meme_roach_video.play();
 })
 
-roach_video.addEventListener("mouseout", function()
+meme_roach_video.addEventListener("mouseout", function()
 {
-  roach_video.pause();
+  meme_roach_video.pause();
 })
 
-function volume_up()
+function volume_change()
 {
-  roach_video.muted = false;
+  meme_roach_video.muted ^= true;
 }
+//
+
+// ㅈㅅㅎㄴㄷ
+meme_sorry_sprite.addEventListener("mousemove", evt =>
+{
+  let y = evt.offsetY / innerHeight;
+  var y_progress = y/0.22;
+  var y_progress_int = Math.floor((y_progress * 10));
+
+  //console.log("progress = " + y_progress);
+  //console.log("y = " + Math.floor((y_progress * 10)));
+
+  if(0 < y_progress_int && 11 > y_progress_int)
+    meme_sorry_sprite.style.backgroundPositionX = (y_progress_int * 10).toString() + "%";
+})
+
+function meme_sorry_play()
+{
+  sfx_sorry.play();
+}
+//
